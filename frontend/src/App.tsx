e03faad03ea085a05f8ca6ed7bcb8dfed1d45e58
Mav3rick <shannonakmeemana11@gmail.com>
2024-08-30 05:52:06 +0530

    FE: initialized tailwind and shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,18 +6,21 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='w-full flex flex-col justify-center item-center'>
+        <div className="flex justify-center">
         <a href="#" target="_blank">
           <img src={garage} className="logo" alt="Vite logo" />
         </a>
         <a href="" target="_blank">
           <img src={cog} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Ride Recap</h1>
+        </div>
+        <h1 className='text-white'>Ride Recap</h1>
       <p className="read-the-docs">
         Work in Progress
       </p>
+      </div>
+      
     </>
   )
 }
